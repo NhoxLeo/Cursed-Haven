0850b42f3f934b9a9c9b1c17159ca5de1db460ad
Anthony Trépanier <anthony.trepanier@hotmail.com>
2020-05-31 22:52:03 -0400

    - add sound script for the UI elements

---
+++ Assets/GAME/Scripts/Game/SoundFx.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFx : MonoBehaviour
+{
+    public void OnClickButtonSound() { 
+    
+        //Mike met ton son de bouton onClick
+    }
+
+    public void OnHoverButtonSound() {
+
+        //Mike met ton son de bouton hover
+
+        Debug.Log("hover");
+
+    }
+}
